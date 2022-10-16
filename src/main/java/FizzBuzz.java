7d41aab177f77443c88f4e1fa5e2de27df43080c
Yuliya Shershen <yuliya@Yuliyas-MacBook-Air.local>
2022-10-16 10:54:51 -0700

    FizzBuzz algo and Tests added

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,25 @@
+public class FizzBuzz {
+
+    public String[] fizzBuzz(int start, int end) {
+        if (start <= end) {
+            String[] array = new String[end - start + 1];
+
+            for (int i = 0; i < array.length && start <= end; i ++) {
+                if (start % 15 == 0) {
+                    array[i] = "FizzBuzz";
+                } else if (start % 3 == 0) {
+                    array[i] = "Fizz";
+                } else if (start % 5 == 0) {
+                    array[i] = "Buzz";
+                } else {
+                    array[i] = String.valueOf(start);
+                }
+                start ++;
+            }
+
+            return array;
+        }
+
+        return new String[0];
+    }
+}
